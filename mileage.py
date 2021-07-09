8116b7b1ec03b4bce46b117f78208f9cbc464d69
ronniekiyegga <ronniekiyegga@hotmail.com>
2021-07-09 08:20:59 +0100

    final input - working with data conversion

--- mileage.py
+++ mileage.py
@@ -1,4 +1,5 @@
 print('How many kilometers did you run');
 km = input();
 miles = float(km)/1.60934;
-print('ok, you ran {} miles'.format(miles));
+rounded_miles = round(miles, 2);
+print('Your {} km, is {} miles'.format(km, rounded_miles));
